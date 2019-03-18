26006b2df3822093122f94092932ff1c6c7dc2e5
stagldnr <stagldnr@debian-java-ldnr.ldnr.local>
2019-03-18 09:51:39 +0100

    Main exo1 MoiButton

---
+++ Events_Entrainements.git/src/events_entrainements/git/Main.java
@@ -0,0 +1,15 @@
+package events_entrainements.git;
+
+/**
+ * @author Marine V
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MoiButton moi = new MoiButton();
+    }
+
+}
