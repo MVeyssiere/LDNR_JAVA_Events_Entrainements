cc6653df481d77e73b1653d9eb2bebe575252a9d
stagldnr <stagldnr@debian-java-ldnr.ldnr.local>
2019-03-18 10:47:50 +0100

    Main exo1 MoiButton creation of a button and add event

--- Events_Entrainements.git/src/events_entrainements/git/MoiButton.java
+++ Events_Entrainements.git/src/events_entrainements/git/MoiButton.java
@@ -6,37 +6,46 @@ import static java.awt.FlowLayout.CENTER;
 import static java.awt.FlowLayout.LEFT;
 import java.awt.Font;
 import java.awt.GridBagLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
 /**
  *
  * @author Marine V
  */
-public class MoiButton {
-
-    JFrame frame;
+public class MoiButton extends JFrame {
     JPanel fenetre;
 
     public MoiButton() {
 
-        frame = new JFrame();
         fenetre = new JPanel();
-
         fenetre.setLayout(new GridBagLayout());
-        fenetre.setPreferredSize(new Dimension(200, 200));
-        // put label MOI and center it in the frame
-        JLabel label = new JLabel("MOI");
-        label.setHorizontalAlignment(LEFT);
-        label.setVerticalAlignment(CENTER);
-        label.setVerticalTextPosition(CENTER);
-        label.setFont(new Font("Monaco", Font.BOLD, 28));
+        // button creation
+        JButton moi = new JButton("MOI");
+        moi.setFont(new Font("Monaco", Font.BOLD, 28)); // to increase font size
+        moi.setPreferredSize(new Dimension(100, 100));
 
-        fenetre.add(label);
-        frame.add(fenetre);
-        frame.pack();
-        frame.setVisible(true);
+        // create the event on a click of the mouse
+        moi.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent me) {
+                JOptionPane.showMessageDialog(null, "Vous avez cliqué", "info!", JOptionPane.WARNING_MESSAGE);
+            }
+        });
+
+
+        //fenetre.add(label);
+        fenetre.add(moi);
+        this.add(fenetre);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        JFrame panneau = new MoiButton();
     }
 }
